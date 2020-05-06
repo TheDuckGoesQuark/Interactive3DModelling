ba77fc83d77f224e04bc073134faf761f8e24fca
Jordan Mackie <jmackie97@hotmail.com>
2020-05-06 19:54:55 +0100

    removed unecessary data

--- src/main/java/stacs/graphics/App.java
+++ src/main/java/stacs/graphics/App.java
@@ -1,8 +1,11 @@
 package stacs.graphics;
+
 import java.nio.IntBuffer;
 
 import org.lwjgl.opengl.GL;
 import org.lwjgl.opengl.GL11;
+import org.lwjgl.opengl.GL20;
+import org.lwjgl.opengl.GL30;
 import org.lwjgl.system.MemoryStack;
 import org.lwjgl.glfw.Callbacks;
 import org.lwjgl.glfw.GLFW;
@@ -23,6 +26,7 @@ public class App {
         init();
         loop();
 
+        // close and terminate when press X
         Callbacks.glfwFreeCallbacks(window);
         GLFW.glfwDestroyWindow(window);
 
@@ -31,24 +35,27 @@ public class App {
     }
 
     public void init() {
+        // Print errors to console
         GLFWErrorCallback.createPrint(System.err).set();
 
-        if(!GLFW.glfwInit()) {
+        if (!GLFW.glfwInit()) {
             throw new IllegalStateException("Unable to initialize GLFW");
         }
 
+        // create GUI window
         GLFW.glfwDefaultWindowHints();
         GLFW.glfwWindowHint(GLFW.GLFW_VISIBLE, GLFW.GLFW_FALSE);
         GLFW.glfwWindowHint(GLFW.GLFW_RESIZABLE, GLFW.GLFW_TRUE);
 
         window = GLFW.glfwCreateWindow(640, 480, "Interactive 3D Modelling", NULL, NULL);
-        if(window == NULL) {
+        if (window == NULL) {
             throw new IllegalStateException("Unable to create GLFW Window");
         }
 
-        GLFW.glfwSetKeyCallback(window, (window, key, scancode, action, mods) -> {});
+        GLFW.glfwSetKeyCallback(window, (window, key, scancode, action, mods) -> {
+        });
 
-        try(MemoryStack stack = stackPush()){
+        try (MemoryStack stack = stackPush()) {
             IntBuffer pWidth = stack.mallocInt(1);
             IntBuffer pHeight = stack.mallocInt(1);
 
@@ -56,7 +63,7 @@ public class App {
 
             GLFWVidMode vidmode = GLFW.glfwGetVideoMode(GLFW.glfwGetPrimaryMonitor());
 
-            GLFW.glfwSetWindowPos(window,(vidmode.width() - pWidth.get(0)) / 2,(vidmode.height() - pHeight.get(0)) / 2);
+            GLFW.glfwSetWindowPos(window, (vidmode.width() - pWidth.get(0)) / 2, (vidmode.height() - pHeight.get(0)) / 2);
 
             GLFW.glfwMakeContextCurrent(window);
             GLFW.glfwSwapInterval(1);
@@ -67,11 +74,21 @@ public class App {
     public void loop() {
         GL.createCapabilities();
 
-        while(!GLFW.glfwWindowShouldClose(window)) {
-            GL11.glClear(GL11.GL_COLOR_BUFFER_BIT|GL11.GL_DEPTH_BUFFER_BIT);
+        float[] vertices = {-0.5f, -0.5f, 0f, 0.5f, -0.5f, 0f, 0f, 0.5f, 0f};
+        int[] indices = {0, 1, 2};
+
+        var mesh = MeshLoader.createMesh(vertices, indices);
+
+        while (!GLFW.glfwWindowShouldClose(window)) {
+            GL11.glClear(GL11.GL_COLOR_BUFFER_BIT | GL11.GL_DEPTH_BUFFER_BIT);
+
+            GL30.glBindVertexArray(mesh.getVaoID());
+            GL20.glEnableVertexAttribArray(0);
+            GL11.glDrawElements(GL11.GL_TRIANGLES, mesh.getVertexCount(), GL11.GL_UNSIGNED_INT, 0);
+            GL20.glDisableVertexAttribArray(0);
+            GL30.glBindVertexArray(0);
 
             GLFW.glfwSwapBuffers(window);
-
             GLFW.glfwPollEvents();
         }
     }
---
+++ src/main/java/stacs/graphics/Mesh.java
@@ -0,0 +1,19 @@
+package stacs.graphics;
+
+public class Mesh {
+    private int vao;
+    private int vertices;
+
+    public Mesh(int vao, int vertex) {
+        this.vao = vao;
+        this.vertices = vertex;
+    }
+
+    public int getVaoID() {
+        return vao;
+    }
+
+    public int getVertexCount() {
+        return vertices;
+    }
+}
