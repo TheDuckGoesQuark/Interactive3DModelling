1d35690af2196c6859246546121a829a1a08fe55
Jordan Mackie <jmackie97@hotmail.com>
2020-05-07 22:35:52 +0100

    tried to add other thing but it broke things and idk dude

--- src/main/java/stacs/graphics/App.java
+++ src/main/java/stacs/graphics/App.java
@@ -1,8 +1,6 @@
 package stacs.graphics;
 
-import stacs.graphics.data.Face;
 import stacs.graphics.data.FaceLoader;
-import stacs.graphics.render.MeshLoader;
 import stacs.graphics.render.Render;
 import stacs.graphics.render.Window;
 
@@ -19,17 +17,16 @@ public class App {
         window = new Window(800, 1000);
         loop();
         window.terminate();
-
     }
 
     public void loop() throws IOException {
-        var faceLoader = new FaceLoader("mesh.csv", "sh_000.csv");
-        var faces = faceLoader.loadFromResources(new String[]{"sh_001.csv", "sh_002.csv", "sh_003.csv"});
+        var faceLoader = new FaceLoader("mesh.csv", "sh_000.csv", "sh_ev.csv");
+        var faces = faceLoader.loadFromResources(new int[]{1, 2, 3});
         var render = new Render();
 
         while (!window.shouldClose()) {
             render.cleanup();
-            render.render(faces[2].getMesh());
+            render.render(faces[0].getMesh());
             window.update();
         }
     }
--- src/main/java/stacs/graphics/data/FaceLoader.java
+++ src/main/java/stacs/graphics/data/FaceLoader.java
@@ -6,10 +6,24 @@ public class FaceLoader {
 
     private final int[] indices;
     private final float[] averageFaceShapeCoords;
+    private final float[] weightings;
 
-    public FaceLoader(String indicesResourceName, String averageFaceShapeCoordinatesResourceName) throws IOException {
+    public FaceLoader(String indicesResourceName, String averageFaceShapeCoordinatesResourceName, String weightingsResourceName) throws IOException {
         indices = loadIndicesFromResource(indicesResourceName);
         averageFaceShapeCoords = loadCoordinatesFromResource(averageFaceShapeCoordinatesResourceName);
+        weightings = loadWeightingsFromResource(weightingsResourceName);
+    }
+
+    private static float[] loadWeightingsFromResource(String weightingsResourceName) throws IOException {
+        var csvResourceLoader = new CSVResourceLoader();
+
+        var weightingStringValues = csvResourceLoader.readToFlatList(weightingsResourceName);
+        var weightings = new float[weightingStringValues.size()];
+        for (int i = 0; i < weightingStringValues.size(); i++) {
+            weightings[i] = Float.parseFloat(weightingStringValues.get(i));
+        }
+
+        return weightings;
     }
 
     private static int[] loadIndicesFromResource(String indicesResourceName) throws IOException {
@@ -33,19 +47,20 @@ public class FaceLoader {
         return vertices;
     }
 
-    public Face loadFromResource(String name) throws IOException {
+    public Face loadFromResource(int faceNumber) throws IOException {
+        var name = String.format("sh_%03d.csv", faceNumber);
         var vertices = loadCoordinatesFromResource(name);
         for (int i = 0; i < vertices.length; i++) {
-            vertices[i] += averageFaceShapeCoords[i];
+            vertices[i] += (averageFaceShapeCoords[i] * weightings[faceNumber]);
         }
         return new Face(vertices, indices);
     }
 
-    public Face[] loadFromResources(String[] strings) throws IOException {
-        var faces = new Face[strings.length];
+    public Face[] loadFromResources(int[] faceNumbers) throws IOException {
+        var faces = new Face[faceNumbers.length];
 
         for (int i = 0; i < faces.length; i++) {
-            faces[i] = loadFromResource(strings[i]);
+            faces[i] = loadFromResource(faceNumbers[i]);
         }
 
         return faces;
